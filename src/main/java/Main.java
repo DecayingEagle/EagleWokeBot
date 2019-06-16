bfde6bfd23d1ff60ece93bcc696222fac450229b
DecayingEagle <therealeagle.yt@gmail.com>
2019-06-16 12:56:25 +0200

    Small changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.core.JDABuilder;
 
 public class Main {
     public static void main(String []args) throws Exception{
-        JDA jda = new JDABuilder("Token").build();
+        JDA jda = new JDABuilder("NTg1NDExMDEzMDEzMjc0NjM3.XQVdpA.2LjtTWv3nIando3UoNltI3tYjZs").build();
         jda.addEventListener(new ping());
         jda.addEventListener(new help());
         jda.addEventListener(new eaglewoke());
